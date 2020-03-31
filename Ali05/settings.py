c6e20e545de65cc3b6e383bb85cac3e89d61051b
Bagaudin Magomedov <51474632+bloodforcream@users.noreply.github.com>
2020-03-31 10:49:42 +0300

    Update settings.py
    updated settings

--- Ali05/settings.py
+++ Ali05/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6qkf4-g1x%4hv45r%2j&ub8kwbcn-dok8=z-c1kz=kafmzttiw'
+SECRET_KEY = ''
 
 with open('config.json') as config_file:
     config = json.load(config_file)
